085dc95ee6f9c0d2d15db31a8f4b15fa828c9ef3
redbean3548 <mihirkumarmk1@gmail.com>
2024-12-07 11:11:41 -0500

    user context

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { AuthProvider } from './contexts/AuthContext'
+import { UserProvider } from './contexts/UserContext.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </AuthProvider>
   </StrictMode>
 )
